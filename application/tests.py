c396800c1c346d7f6fa560a6dbbfb0508970d4ba
WheresAlice <WheresAlice@users.noreply.github.com>
2021-02-07 16:32:37 +0000

    fix tests

--- application/tests.py
+++ application/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
-from .models import Subscriber
+from .models import Member
 
 
 # Create your tests here.
 class UserTests(TestCase):
-    def test_create_user(self):
-        user = Subscriber(phone_number='+1202-555-0483', name='Fake User')
+    def test_create_member(self):
+        user = Member(phone_number='+1202-555-0483', name='Fake User')
         user.save()
