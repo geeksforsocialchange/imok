e0d7384ccae7ca386ae9645588b323c29a9ef705
WheresAlice <WheresAlice@users.noreply.github.com>
2021-03-11 18:50:10 +0000

    Make checkins visible in admin page

--- application/admin.py
+++ application/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from imok.settings import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_FROM_NUMBER
+from imok.settings import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_FROM_NUMBER, CHECKIN_TTL
 from twilio.rest import Client
 from django.utils.translation import gettext as _
+import django.utils.timezone as timezone
 
 from .models import Checkin, Member
-
 
 def send_invite(obj):
     print(f"Sending an invite to {obj.phone_number.as_e164}")
@@ -31,4 +31,39 @@ class MemberAdmin(admin.ModelAdmin):
         obj.save()
 
 
+class CheckinAdmin(admin.ModelAdmin):
+    list_display = ['checkin_user', 'checkin_phone_number', 'time_stamp', 'overdue', 'is_ok']
+    list_filter = ['member__is_ok']
+    list_select_related = True
+    search_fields = ['member__name', 'member__phone_number']
+    empty_value_display = 'unknown'
+    list_display_links = None
+    ordering = ['time_stamp']
+
+    def checkin_user(self, obj):
+        return f"{obj.member.name}"
+
+    def checkin_phone_number(self, obj):
+        return obj.member.phone_number
+
+    def overdue(self, obj):
+        return obj.time_stamp < timezone.now() - CHECKIN_TTL
+
+    def is_ok(self, obj):
+        return obj.member.is_ok
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    overdue.boolean = True
+    is_ok.boolean = True
+
+
 admin.site.register(Member, MemberAdmin)
+admin.site.register(Checkin, CheckinAdmin)
