f358a3f7d222db15af75a57185b99293f4d5d525
WheresAlice <WheresAlice@users.noreply.github.com>
2021-04-16 19:26:47 +0100

    expose config to superusers

--- public/urls.py
+++ public/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
+
 from . import views
 
 app_name = 'public'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('healthz', views.healthz, name='healthz')
+    path('healthz', views.healthz, name='healthz'),
+    path('varz', views.varz, name='varz'),
 ]
--- public/views.py
+++ public/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
 from logging import getLogger
+from os import getenv
+import re
+
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse, HttpResponseForbidden
 
 from application.models import Member
 
@@ -16,6 +20,42 @@ def index(request):
 def healthz(request):
     if database_ok():
         return HttpResponse("OK")
+
+
+def varz(request):
+    if request.user.is_superuser:
+        response = {
+            'ALLOWED_HOSTS': settings.ALLOWED_HOSTS,
+            'TWILIO_ACCOUNT_SID': redact(settings.TWILIO_ACCOUNT_SID),
+            'TWILIO_AUTH_TOKEN': redact(settings.TWILIO_AUTH_TOKEN),
+            'TWILIO_FROM_NUMBER': redact(settings.TWILIO_FROM_NUMBER),
+            'TELEGRAM_TOKEN': redact(settings.TELEGRAM_TOKEN),
+            'TELEGRAM_GROUP': redact(settings.TELEGRAM_TOKEN),
+            'DOKKU_LETSENCRYPT_EMAIL': getenv('DOKKU_LETSENCRYPT_EMAIL'),
+            'NOTIFY_EMAIL': redact(getenv('NOTIFY_EMAIL')),
+            'MAIL_FROM': redact(getenv('MAIL_FROM')),
+            'EMAIL_HOST': getenv('EMAIL_HOST'),
+            'EMAIL_PORT': getenv('EMAIL_PORT'),
+            'EMAIL_USE_TLS': getenv('EMAIL_USE_TLS'),
+            'EMAIL_HOST_USER': redact(getenv('EMAIL_HOST_USER')),
+            'EMAIL_HOST_PASSWORD': redact(getenv('EMAIL_HOST_PASSWORD'))
+        }
+        if hasattr(settings, 'AIRBRAKE_PROJECT'):
+            response['AIRBRAKE_PROJECT'] = redact(settings.AIRBRAKE_PROJECT)
+            response['AIRBRAKE_PROJECT_KEY'] = redact(settings.AIRBRAKE_PROJECT_KEY)
+        return JsonResponse(response)
+    else:
+        return HttpResponseForbidden('{"ERROR": "Not authenticated"}')
+
+
+def redact(string):
+    string = str(string)
+    if len(string) < 4:
+        return re.sub(r".", "*", string)
+    first_char = string[0]
+    last_char = string[-1]
+    modified_str = re.sub(r".", "*", string[1:-1])
+    return first_char + modified_str + last_char
 
 
 def database_ok():
