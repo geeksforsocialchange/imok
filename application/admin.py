bd981061d47f59777f06554b8ba9758efdd085ec
WheresAlice <WheresAlice@users.noreply.github.com>
2021-05-07 20:16:00 +0100

    chore: restore ability to delete checkins

--- application/admin.py
+++ application/admin.py
@@ -105,9 +105,6 @@ class CheckinAdmin(admin.ModelAdmin):
     def has_change_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
-
     overdue.boolean = True
     is_ok.boolean = True
 
